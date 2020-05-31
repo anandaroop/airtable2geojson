f267c36be217993ddf65ab012fd8ccd157d4aabe
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-05-30 21:25:03 -0400

    Easier sample query

--- dev/server.ts
+++ dev/server.ts
@@ -10,7 +10,37 @@ app.use(bodyParser.json())
 app.get("/", (req, res) => {
   res.send(`
   <html>
-    <a href="/airtableToGeoJSON?tableName=Coordinated%20delivery%20shifts&idFieldName=Unique%20ID&geocodedFieldName=Geocode%20cache">Sample /airtableToGeoJSON query</a>
+    <form action="/airtableToGeoJSON">
+      <div style="margin-bottom: 1em">
+        <label style="display: inline-block; min-width: 9em">
+          Table name
+        </label>
+        <input type="text" name="tableName" value="Volunteers" />
+      </div>
+
+      <div style="margin-bottom: 1em">
+        <label style="display: inline-block; min-width: 9em">
+          View name
+        </label>
+        <input type="text" name="viewName" value="Map" />
+      </div>
+
+      <div style="margin-bottom: 1em">
+        <label style="display: inline-block; min-width: 9em">
+          ID field
+        </label>
+        <input type="text" name="idFieldName" value="Unique ID" />
+      </div>
+
+      <div style="margin-bottom: 1em">
+        <label style="display: inline-block; min-width: 9em">
+          Geocode cache field
+        </label>
+        <input type="text" name="geocodedFieldName" value="Geocode cache" />
+      </div>
+
+      <input type="submit" />
+    </form>
   </html>
   `)
 })
@@ -18,7 +48,7 @@ app.get("/airtableToGeoJSON", airtableToGeoJSON)
 app.post("/airtableToGeoJSON", airtableToGeoJSON)
 app.options("/airtableToGeoJSON", airtableToGeoJSON)
 
-const port = 3000
+const port = 3001
 app.listen(port, () =>
   console.log(`Dev server listening at http://localhost:${port}`)
 )
